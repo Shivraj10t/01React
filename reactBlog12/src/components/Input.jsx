a2f92e7b4e9461c1e849d875cdc6663b4878ef67
shivraj tiwaskar <shivrajtiwaskar7@gmail.com>
2024-02-25 01:09:27 +0530

    working on form

---
+++ reactBlog12/src/components/Input.jsx
@@ -0,0 +1,32 @@
+import { useId } from "react";
+import { forwardRef } from "react";
+
+const Input = forwardRef(function Input(
+  { lable, type, className = "", ...props },
+  ref
+) {
+  const id = useId();
+  return (
+    <div className="w-full">
+      {lable && (
+        <lable className="inline-block mb-1 pl-1" htmlFor={id}>
+          {lable}
+        </lable>
+      )}
+      <input
+        id={id}
+        className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+        type={type}
+        {...props}
+        ref={ref}
+      />
+    </div>
+  );
+});
+
+// {
+//   const id = useId();
+//   return <input id={id} />;
+// };
+
+export default Input;
--- reactBlog12/src/components/index.js
+++ reactBlog12/src/components/index.js
@@ -1,4 +1,9 @@
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
-
-export { Header, Footer };
+import Logo from "./Logo";
+import LogoutBtn from "./header/LogoutBtn";
+import Container from "./container/Container";
+import Input from "./Input";
+import PostCard from "./PostCard";
+import Button from "./Button";
+export { Header, Footer, Logo, LogoutBtn, Container, Input, PostCard, Button };
